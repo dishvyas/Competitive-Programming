58430f520567a464bdffb2b90cd4636b4321cc13
dishvyas <33374803+dishvyas@users.noreply.github.com>
2018-11-05 22:24:17 +0530

    Create NOV16B2.cpp

---
+++ NOV16B2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,a,x=0,y=0,i,b=0;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        cin>>a;
+        x=1;
+        y=pow(2,a);
+        cout<<x<<" "<<y<<" ";
+    }
+	return 0;
+}
