46f64805a49377f7691de87bb616b407166135bf
dishvyas <33374803+dishvyas@users.noreply.github.com>
2018-11-13 00:35:49 +0530

    Update NOV16B2.cpp

--- NOV16B2.cpp
+++ NOV16B2.cpp
@@ -1,14 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
+int Jacobsthal(int n) 
+{ 
+    if (n == 0) 
+        return 0; 
+    if (n == 1) 
+        return 1; 
+    return Jacobsthal(n - 1) + 2 * Jacobsthal(n - 2); 
+} 
 int main()
 {
-    int t,a,x=0,y=0,i,b=0;
+    int t,a,i;
+    long long int x,y;
     cin>>t;
     for(i=0;i<t;i++)
     {
         cin>>a;
-        x=1;
+        x=Jacobsthal(a);
         y=pow(2,a);
         cout<<x<<" "<<y<<" ";
     }
